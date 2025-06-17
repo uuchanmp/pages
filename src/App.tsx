298c4a89aa402a734f1770a0933374aca6df2654
UU Chanmp <dr.chitkolinn@yahoo.com>
2025-06-17 20:26:29 +0630

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://uuchanmp.xmit.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://uuchanmp.xmit.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>UUCHANMP.XMIT.DEV</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
